7fc2a03e96078d7ff6d105187b3c1da99b05ec3f
Luiz Henrique <luizhjramos@outlook.com>
2021-12-18 19:53:09 -0300

    feat: createGlobalStyles in the application

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
+// Packages
 import React from "react";
 import ReactDOM from "react-dom";
+
+// Components
 import App from "./App";
+
+// GlobalStyles
+import GlobalStyles from "./styles/GlobalStyles";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
