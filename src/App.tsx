c94658bdeada6c08eecc2f58f2a0bf9d66bd7341
Luiz Henrique <luizhjramos@outlook.com>
2021-12-18 19:30:53 -0300

    chore: add  styled-component/types

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+// Packages
 import React from "react";
 
+// Components
 import Home from "./pages/Home";
+
+// Theme
+import theme from "./styles/theme/theme";
+import {} from "styled-components";
 
 function App() {
   return <Home />;
