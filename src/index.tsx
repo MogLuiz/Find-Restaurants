0570adfd1f7ad7507c2811f5f1e4ec63055a6d74
Luiz Henrique <luizhjramos@outlook.com>
2021-12-19 13:35:58 -0300

    chore: add react-slick

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,8 @@ import App from "./App";
 import GlobalStyles from "./styles/GlobalStyles";
 import "@material/react-text-field/dist/text-field.css";
 import "@material/react-material-icon/dist/material-icon.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
