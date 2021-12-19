1a06fe29754f89409d3f2f1812735136f5ec1923
Luiz Henrique <luizhjramos@outlook.com>
2021-12-18 22:15:03 -0300

    feat: search input

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,35 @@
 // Packages
-import React from "react";
+import React, { useState } from "react";
 
 // Assets
 import logo from "../../assets/logo.svg";
+
+// Components
+import TextField, { Input } from "@material/react-text-field";
 
 // Styles
 import { Container, Search } from "./styles";
 
 const Home: React.FC = () => {
+  // -------------------------------------------------
+  // States
+  // -------------------------------------------------
+
+  const [searchInputValue, setSearchInputValue] = useState("");
+
+  // -------------------------------------------------
+  // Render
+  // -------------------------------------------------
   return (
     <Container>
       <Search>
         <img src={logo} alt="Logo da aplicação" />
+        <TextField label="Pesquisar" outlined>
+          <Input
+            value={searchInputValue}
+            onChange={(e: any) => setSearchInputValue(e.target.value)}
+          />
+        </TextField>
       </Search>
     </Container>
   );
