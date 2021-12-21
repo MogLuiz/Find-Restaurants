61d2544d2cc7b894b4ce4ed3ff9ab7847bc99e14
Luiz Henrique <luizhjramos@outlook.com>
2021-12-20 21:13:21 -0300

    feat: Initializing Modal component with React Portal provider

--- src/components/Modal/Portal/index.tsx
+++ src/components/Modal/Portal/index.tsx
@@ -1,7 +1,10 @@
 // Packages
+import { ReactNode } from "react";
+
+// Types
 import ReactDom from "react-dom";
 
-const PortalModal: React.FC<JSX.Element> = ({ children }) => {
+const PortalModal: React.FC<ReactNode> = ({ children }) => {
   const portal = document.getElementById("modal-root") as HTMLElement;
   return ReactDom.createPortal(children, portal);
 };
---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,15 @@
+// Packages
+import React from "react";
+
+// React Portal
+import PortalModal from "./Portal";
+
+const Modal: React.FC = () => {
+  return (
+    <PortalModal>
+      <div>x</div>
+    </PortalModal>
+  );
+};
+
+export default Modal;
