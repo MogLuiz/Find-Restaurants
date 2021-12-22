24a91f63fab0dbe6adda1702d5e4df4c0ac8d8e5
Luiz Henrique <luizhjramos@outlook.com>
2021-12-21 23:28:09 -0300

    feat: SearchNearby function to search nearby restaurants

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -5,7 +5,34 @@ import React from "react";
 import { GoogleApiWrapper, Map, Marker } from "google-maps-react";
 
 export const MapContainer: React.FC<any> = (props) => {
+  // -------------------------------------------------
+  // Props
+  // -------------------------------------------------
   const { google } = props;
+
+  // -------------------------------------------------
+  // Functions
+  // -------------------------------------------------
+
+  const searchNearby = (map: any, center: any) => {
+    const service = new google.maps.placesService(map);
+
+    const request = {
+      location: center,
+      radius: "20000",
+      type: ["restaurant"],
+    };
+
+    service.nearbySearch(request, (results: any, status: any) => {
+      if (status === google.maps.places.placesService.OK) {
+        console.log({ results });
+      }
+    });
+  };
+
+  // -------------------------------------------------
+  // Render
+  // -------------------------------------------------
   return <Map google={google} centerAroundCurrentLocation />;
 };
 
