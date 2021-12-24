b418b09d667496d01e728483b413babc4e14647a
Luiz Henrique <luizhjramos@outlook.com>
2021-12-24 12:00:33 -0300

    feat: new module architecture

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -2,7 +2,7 @@
 import { combineReducers } from "redux"
 
 // Reducers
-import restaurants from "../modules/restaurants";
+import restaurants from "../modules/restaurants/reducer";
 
 export default combineReducers({
     restaurants,
