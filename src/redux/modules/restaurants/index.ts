8eb7b6c9d6ba1859f126a83b08ef9312a31dfe00
Luiz Henrique <luizhjramos@outlook.com>
2021-12-24 11:32:08 -0300

    feat: initialize the restaurants reducer

--- src/redux/modules/restaurants/index.ts
+++ src/redux/modules/restaurants/index.ts
@@ -1,7 +1,24 @@
 // Types
 import { ActionTypes } from "./types"
+import { Reducer } from "redux"
 
 const INITIAL_STATE = {
     restaurants: [],
     restaurantSelected: null,
 }
+
+const restaurants: Reducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case ActionTypes.SET_RESTAURANTS: 
+            break;
+
+        case ActionTypes.SET_SELECTED_RESTAURANT:
+            break;
+
+        default:
+            break;
+        
+    }
+}
+
+export default restaurants;
