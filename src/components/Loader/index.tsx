48b6b6151373e9f005cdef8c87e479b3f9f27df4
Luiz Henrique <luizhjramos@outlook.com>
2022-01-03 20:55:55 -0300

    feat: initialize the loader component

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,12 @@
+// Packages
+import React from "react";
+import Lottie from "react-lottie";
+
+// Assets
+import animationData from "../../assets/restaurants-loading.json";
+
+const Loader: React.FC = () => {
+  return <div />;
+};
+
+export default Loader;
