167de85269afb7792a8067e37b75dd7ab49bea07
Luiz Henrique <luizhjramos@outlook.com>
2022-01-04 19:16:14 -0300

    feat: creating the instance of an image and assigning the onLoader to it

--- src/components/ImageCard/index.tsx
+++ src/components/ImageCard/index.tsx
@@ -1,5 +1,5 @@
 // Packages
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // Assets
 import restaurants from "../../assets/restaurante-fake.png";
@@ -14,15 +14,33 @@ interface IImageCardProps {
   restaurant: IEstablishmentsSearched;
 }
 
-const ImageCard: React.FC<IImageCardProps> = ({ restaurant }) => (
+const ImageCard: React.FC<IImageCardProps> = ({ restaurant }) => {
+  // -------------------------------------------------
+  // State
+  // -------------------------------------------------
+
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+  // -------------------------------------------------
+  // Hooks
+  // -------------------------------------------------
+
+  useEffect(() => {
+    const imageLoader = new Image();
+    imageLoader.src = restaurant?.photos[0];
+    imageLoader.onload = () => setIsImageLoaded(true);
+  }, [restaurant.photos[0]]);
+
   // -------------------------------------------------
   // Render
   // -------------------------------------------------
-  <Card
-    photo={restaurant.photos ? restaurant?.photos[0]?.getUrl() : restaurants}
-  >
-    <Title>{restaurant.name}</Title>
-  </Card>
-);
+  return (
+    <Card
+      photo={restaurant.photos ? restaurant?.photos[0]?.getUrl() : restaurants}
+    >
+      <Title>{restaurant.name}</Title>
+    </Card>
+  );
+};
 
 export default ImageCard;
